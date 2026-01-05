484fee96fa8e70097910ce20c6c0da46eef84c3e
StaceyO21 <staceyobrien21@gmail.com>
2026-01-04 19:17:00 -0600

    user controller

---
+++ backend/demo/src/main/java/org/launchcode/route_runner/RouteRunnerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.route_runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteRunnerApplication.class, args);
+	}
+
+}
--- src/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Mainpage from "./Components/Mainpage.jsx";
-
-export default function App() {
-  return <Mainpage />;
-}
