6b58dad581c8f4038c65163df3b8f20adc3ab245
StaceyO21 <staceyobrien21@gmail.com>
2026-01-18 20:32:30 -0600

    dto

---
+++ backend/demo/src/main/java/org/launchcode/route_runner/dto/ProductsDTO.java
@@ -0,0 +1,18 @@
+package org.launchcode.route_runner.dto;
+
+public class ProductsDTO {
+
+    private String name;
+
+    private Double price;
+
+    private String description;
+
+    public productsDTO(String name, Double price, String description){
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+
+}
