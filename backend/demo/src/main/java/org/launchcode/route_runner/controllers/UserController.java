02d50c40076d3db1e04fe3633655161ff36baadc
StaceyO21 <staceyobrien21@gmail.com>
2026-01-24 21:01:14 -0600

    userservice

--- backend/demo/src/main/java/org/launchcode/route_runner/controllers/UserController.java
+++ backend/demo/src/main/java/org/launchcode/route_runner/controllers/UserController.java
@@ -1,11 +1,16 @@
 package org.launchcode.route_runner.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
+
+    @Autowired UserService userService;
+
+
 
 
 }
