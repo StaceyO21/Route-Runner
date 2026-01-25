2ec87de09955ae5283ca4bce78eba8f7b1fe5b3b
StaceyO21 <staceyobrien21@gmail.com>
2026-01-24 21:34:11 -0600

    setting up create user controller

--- backend/demo/src/main/java/org/launchcode/route_runner/controllers/UserController.java
+++ backend/demo/src/main/java/org/launchcode/route_runner/controllers/UserController.java
@@ -1,7 +1,11 @@
 package org.launchcode.route_runner.controllers;
 
+import org.hibernate.mapping.Map;
 import org.launchcode.route_runner.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +16,19 @@ public class UserController {
     @Autowired
     UserService userService;
 
+@PostMapping("/register")
+    public ResponseEntity<?> createUser(@RequestBody Map<String, String> userData){
+    try {
+        String email = userData.get("email");
+        String username = userData.get("username");
+        String password = userData.get("password");
+        String verify = userData.get("verify");
 
+        if(!password.equals(verify)) {
+            return ResponseEntity.badRequest().body(Map.of("error", "Passwords do not match"));
+        }
+    }
+}
 
 
 }
